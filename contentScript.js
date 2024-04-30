8f756f6e2c738341aa12c067903ca81a78d164e7
EliDevToBe <maxime.ktr@proton.me>
2024-04-30 15:58:48 +0200

    branche test

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,29 @@
-console.log("Hello!");
+setTimeout(() => {
+    console.log("Hello!");
+
+    const tomate = document.querySelector("h1");
+    const html = document.querySelector("html");
+    const head = document.querySelector("head")
+    const initialStyle = document.querySelector("")
+
+    const newStyle = document.createElement("style")
+    newStyle.setAttribute("id", "onetrust-style")
+    newStyle.append("#onetrust-reject-all-handler:{color:pink}")
+
+    head.append(newStyle)
+
+    tomate.classList.add("cookiesAway");
+
+    // if (html.innerHTML.includes("onetrust")) {
+    //     console.log("inside")
+
+    //     let onetrust = document.querySelector("#onetrust-style")
+    //     onetrust.append("#onetrust-reject-all-handler:{color:pink}")
+
+    // }
+}
+    , 2000);
 // "matches": [
-    //"https://*.carrefour.fr/*"
-    //"<all_urls>"
+//"https://*.carrefour.fr/*"
+//"<all_urls>"
 //]
