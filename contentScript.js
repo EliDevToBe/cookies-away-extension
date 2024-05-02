3fde9ead170c32997238436a09786bb916f931c1
Mialy333 <mialy.ratsimbazafy@gmail.com>
2024-05-02 16:32:58 +0200

    code js fonction cookiesAway + all selectors

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,35 @@
-setTimeout(() => {
-    console.log("Hello!");
+console.log("Hello!");
 
-    const rejectAll = document.querySelector("#onetrust-reject-all-handler");
-    rejectAll.setAttribute("style", "color:pink !important; font-size:50px !important;background-color:black !important")
-
+function cookiesAway(selector) {
+    selector.setAttribute("style", "color:pink !important; font-size:50px !important;background-color:black !important")
 }
-    , 2000);
+//----------------------------------------------------------------------------------
 
-lol 
+window.onload = function () {
+    setTimeout(() => {
+        let oneTrustId = document.getElementById("onetrust-reject-all-handler");
+        let oneTrustClass = document.querySelector(".onetrust-reject-all-handler");
+        let oneTrustClass2 = document.querySelector(".onetrust-close-btn-handler");
+        let didomiClass = document.querySelector(".didomi-continue-without-agreeing");
+        let didomiPaywall = document.querySelector(".didomi-popup-open");
+
+        let arraySelector = [oneTrustId, oneTrustClass, didomiClass, oneTrustClass2, didomiPaywall];
+
+        for (let i = 0; i < arraySelector.length; i++) {
+            const element = arraySelector[i];
+            try { // Ici on essaie tout ce qu'on veut faire fonctionner. Si erreur, alors on bascule dans catch(error) sans stopper tout le code.
+                cookiesAway(element);
+
+            } catch (error) {
+                console.log("pas d'element");
+                let paywallFinder = document.querySelector("body").innerHTML.includes("paywall")
+                if (paywallFinder) {
+
+                    alert("Ce site te prend en otage !!!")
+                }
+
+            }
+
+        }
+    }, 500);
+}
