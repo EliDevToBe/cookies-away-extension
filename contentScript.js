2d674c53ead670403681591de50ac1f976fe4eb6
Mialy333 <mialy.ratsimbazafy@gmail.com>
2024-05-02 10:42:34 +0200

    code cleaned

--- contentScript.js
+++ contentScript.js
@@ -1,24 +1,8 @@
 setTimeout(() => {
     console.log("Hello!");
 
-    // const h1 = document.querySelector("h1")
-    // h1.setAttribute("style", "color:pink");
-
     const rejectAll = document.querySelector("#onetrust-reject-all-handler");
     rejectAll.setAttribute("style", "color:pink !important; font-size:50px !important;background-color:black !important")
 
-    // if (html.innerHTML.includes("onetrust")) {
-    //     console.log("inside")
-
-    // let onetrust = document.querySelector("#onetrust-style")
-    // onetrust.append("#onetrust-reject-all-handler:{color:pink !important}")
-
-    // }
-
-    // test mialy
 }
     , 2000);
-// "matches": [
-//"https://*.carrefour.fr/*"
-//"<all_urls>"
-//]
